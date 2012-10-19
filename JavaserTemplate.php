2226b62454b4b3453946e42c3feaced79282f1b3
Bo Thinggaard <bo@tnactas.dk>
2012-10-19 14:20:32 +0200

    Java serialization template, for use with a basic serialization interface.

--- JavaserTemplate.php
+++ JavaserTemplate.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/ITemplate.php';
  *
  * @author bo@unpossiblesystems.dk
  */
-class JavaTemplate implements ITemplate {
+class JavaserTemplate implements ITemplate {
     private static $_types = array(
         Builder::TYPE_STRING => 'String',
         Builder::TYPE_INT => 'int',
@@ -23,9 +23,10 @@ class JavaTemplate implements ITemplate {
     );
 
     public function getClass() {
-        $snippet = "public class {classname} {\n";
+        $snippet = "public class {classname} implements JsonSerializable {\n";
         $snippet .= "{properties}";
         $snippet .= "{methods}";
+		$snippet .= "{extras}";
         $snippet .= "}";
         
         return $snippet;
@@ -55,6 +56,66 @@ class JavaTemplate implements ITemplate {
     public function getFileExtension() {
         return ".java";
     }
+
+	public function getExtras($properties, $class) {
+		
+		$snippet  = "\n\t@Override\n";
+		$snippet .= "\tpublic JSONObject JsonSerialize() throws JSONException {\n";
+		$snippet .= "\t\tJSONObject json = new JSONObject();\n";
+		
+		foreach ($properties as $property) {
+			$snippet .= $this->_getPut($property);
+		}
+		
+		$snippet .= "\t\treturn json;\n";
+		$snippet .= "\n\t}\n";
+		
+		
+		$snippet .= "\n\t@Override\n";
+		$snippet .= "\tpublic $class JsonDeserialize(JSONObject json) throws JSONException {\n";
+		$snippet .= "\t\tif (json != null) {;\n";
+		
+		foreach ($properties as $property) {
+			$snippet .= $this->_getOpt($property);
+		}
+		
+		$snippet .= "\t\t}\n";
+		$snippet .= "\t\treturn this;\n";
+		$snippet .= "\t}\n";
+		
+		return $snippet;
+	}
+	
+	private function _getPut($property) {
+		switch ($property['{type}']) {
+			case self::$_types[Builder::TYPE_STRING]:
+			case self::$_types[Builder::TYPE_INT]:
+			case self::$_types[Builder::TYPE_FLOAT]:
+			case self::$_types[Builder::TYPE_BOOL]:
+				return "\t\tjson.put(\"{$property['{name.lc}']}\", m{$property['{name.uc}']})\n";
+			case substr($property['{type}'], 0, 4) != 'List':
+				return "\t\tjson.put(\"{$property['{name.lc}']}\", m{$property['{name.uc}']}.JsonSerialize())\n";
+			default:
+				return "\t\t//m{$property['{name.uc}']} is a LIST!\n";
+		}
+	}
+	
+	private function _getOpt($property) {
+		switch ($property['{type}']) {
+			case self::$_types[Builder::TYPE_STRING]:
+				return "\t\t\tm{$property['{name.uc}']} = json.optString(\"{$property['{name.lc}']}\");\n";
+			case self::$_types[Builder::TYPE_INT]:
+				return "\t\t\tm{$property['{name.uc}']} = json.optInt(\"{$property['{name.lc}']}\");\n";
+			case self::$_types[Builder::TYPE_FLOAT]:
+				return "\t\t\tm{$property['{name.uc}']} = json.optDouble(\"{$property['{name.lc}']}\");\n";
+			case self::$_types[Builder::TYPE_BOOL]:
+				return "\t\t\tm{$property['{name.uc}']} = json.optBoolean(\"{$property['{name.lc}']}\");\n";
+			case substr($property['{type}'], 0, 4) != 'List':
+				return "\t\t\tm{$property['{name.uc}']} = {$property['{type}']}.JsonDeserialize(json.optJSONObject(\"{$property['{name.lc}']}\"));\n";
+			default:
+				return "\t\t\t//m{$property['{name.uc}']} is a LIST!\n";
+		}
+	}
 }
 
 ?>
